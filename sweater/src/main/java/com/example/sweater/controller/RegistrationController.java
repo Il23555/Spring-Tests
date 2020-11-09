8bf0d689ca71366585508287a33aaaf7310078f4
Fakhrtdinova Ilyuza <48441714+Il23555@users.noreply.github.com>
2020-11-09 22:36:55 +0300

    add registration and tests

---
+++ sweater/src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -0,0 +1,58 @@
+package com.example.sweater.controller;
+
+import com.example.sweater.domain.Role;
+import com.example.sweater.domain.User;
+import com.example.sweater.repos.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Collections;
+import java.util.Map;
+
+@Controller
+public class RegistrationController {
+    @Autowired
+    private UserRepo userRepo;
+
+    public RegistrationController(){}
+
+    @GetMapping("/registration")
+    public String registration() {
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(User user, Map<String, Object> model) {
+        User userFromDb = userRepo.findByUsername(user.getUsername());
+        if (userFromDb != null) {
+            model.put("message", "User exists!");
+            return "registration";
+        }
+
+        if (!isValidPassword(user.getPassword())){
+            model.put("message","This password must be more then 8 characters.");
+            return "registration";
+        }
+
+        if (!isValidLogin(user.getUsername())){
+            model.put("message","This login must be more then 3 characters!");
+            return "registration";
+        }
+
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepo.save(user);
+
+        return "redirect:/login";
+    }
+
+    public boolean isValidPassword(String password){
+        return password.length() >= 8;
+    }
+
+    public boolean isValidLogin(String login){
+        return login.length() >= 3;
+    }
+}
---
+++ sweater/src/test/java/com/example/sweater/controller/RegistrationControllerTest.java
@@ -0,0 +1,50 @@
+package com.example.sweater.controller;
+
+import com.example.sweater.domain.User;
+
+import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SpringBootTest
+class RegistrationControllerTest {
+
+    @Autowired
+    RegistrationController registrationController;
+
+    Map<String,Object> model = new HashMap<>();
+
+    @Test
+    void addUser() {
+        User user1 = new User();
+        user1.setUsername("ilyuza");
+        user1.setPassword("12345");
+        registrationController.addUser(user1, model);
+        Assert.assertEquals("User exists!", model.get("message"));
+
+        User user2 = new User();
+        user2.setUsername("maria");
+        user2.setPassword("u8956789");
+        Assert.assertEquals("redirect:/login", registrationController.addUser(user2, model));
+
+        Assert.assertEquals("registration", registrationController.addUser(user2, model));
+    }
+
+    @Test
+    void isValidPassword() {
+        RegistrationController registrationController = new RegistrationController();
+        Assert.assertFalse(registrationController.isValidPassword("abc"));
+        Assert.assertTrue(registrationController.isValidPassword("aaaabbbcc"));
+    }
+
+    @Test
+    void isValidLogin() {
+        RegistrationController registrationController = new RegistrationController();
+        Assert.assertFalse(registrationController.isValidLogin("12"));
+        Assert.assertTrue(registrationController.isValidLogin("john"));
+    }
+}
